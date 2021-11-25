8b747c7fe75f9c80df824b298c85c0b829ef1df3
Aadvik <aadv1k@outlook.com>
2021-11-25 14:36:07 +0530

    Added a html string parser

--- chad.js
+++ chad.js
@@ -1,8 +1,10 @@
 window.onload = () => {
   // the chad way
-  chad("#app").append("<h1>Hello chad</h1>");
-  // Noob way
-  // document.getElementById("app").innerHTML += "<h1>Hello chad</h1>";
+  app = chad("#app");
+  aloha = prompt("Sike");
+  app.append(
+    `<div id='hmm' class='foo' style='background: gray;'>${aloha}</div>`
+  );
 };
 
 // TODO Implement more selectors
@@ -11,17 +13,50 @@ function chad(element) {
     return document.getElementById(element.substring(1));
   } else if (element[0] == ".") {
     return document.getElementsByClassName(element.substring(1));
-  } else if (element[0] == "<") {
-    tag = element.substring(element.indexOf("<") + 1, element.indexOf(">"));
-    return document.getElementsByTagName(tag);
   } else {
-    return document.getElementById(element);
+    return document.getElementsByTagName(element);
   }
 }
 
+function parseHtmlString(htmlString) {
+  tag = htmlString.substring(
+    htmlString.indexOf("<") + 1,
+    htmlString.indexOf(" ")
+  );
+  console.log(tag);
+
+  dict = { atribs: [] };
+  parser = new DOMParser();
+  parsedHtml = parser
+    .parseFromString(htmlString, "text/html")
+    .getElementsByTagName(tag)[0];
+
+  dict["innerText"] = parsedHtml.innerText;
+  dict["tag"] = tag;
+
+  for (atrib of parsedHtml.getAttributeNames()) {
+    dict["atribs"].push({
+      item: atrib,
+      value: parsedHtml.getAttribute(atrib),
+    });
+  }
+  return dict;
+}
+
 // DOM insertion
+app.append = function (string) {
+  data = parseHtmlString(string);
+  child = document.createElement(data["tag"]);
+  child.innerText = data["innerText"];
+  for (atrib of data["atribs"]) {
+    child.setAttribute(atrib["item"], atrib["value"]);
+  }
+
+  this.appendChild(child);
+};
+
 app.prepend = function (string) {
-  text = this.innerHTML;
+  result = document.createChild("");
 
   if (typeof string == "string") {
     this.innerHTML = text.substring(0, 0) + string + text.substring();
@@ -30,16 +65,8 @@ app.prepend = function (string) {
   }
 };
 
-app.append = function (string) {
-  if (typeof string == "string") {
-    this.innerHTML += string;
-  } else {
-    this.innerHTML += string.innerHTML;
-  }
-};
-
 // Class attribute manipulation
-app.setClass = function (className) {
+app.addClass = function (className) {
   if (checkArray(className)) {
     classNames = [...className];
   } else {
@@ -68,6 +95,7 @@ app.hasClass = function (className) {
 };
 
 app.toggleClass = function (className) {
+  let classNames;
   if (checkArray(className)) {
     classNames = [...className];
   } else {
@@ -76,6 +104,11 @@ app.toggleClass = function (className) {
   classNames.forEach((e) => {
     this.classList.toggle(e);
   });
+};
+
+// DOM removal
+app.unwrap = function () {
+  return this.innerHTML;
 };
 
 function checkArray(arr) {
